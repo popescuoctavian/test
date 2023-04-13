805bc83f4ce6e3723277c064c17cc426f901c2d1
Octavian Popescu <popescu.octavian09@gmail.com>
2023-04-13 23:55:01 +0300

    added code

---
+++ test/main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+// convert a string to a number
+int toNumber(const std::string& str) {
+    int result = 0;
+    for (int i = 0; i < str.length(); i++) {
+        result = result * 10 + (str[i] - '0');
+    }
+    return result;
+}
+
+int main() {
+    std::string str;
+    std::cin >> str;
+    std::cout << toNumber(str) << std::endl;
+    return 0;
+}
